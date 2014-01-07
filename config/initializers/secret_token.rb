5b819c363660a81273e49fa146bc3bcad2acc129
Volmer <git@radicaos.com>
2014-01-07 13:36:39 -0200

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BootsyDemo::Application.config.secret_key_base = '5aeb7e5a58d10de2c9c29c50072a0011ab7c3c19c0d98a2436527833032de2c5b13fedbb963a6fea31b693e988565645205f070f3a9c308f4c13238688b19008'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BootsyDemo::Application.config.session_store :cookie_store, key: '_bootsy-demo_session'
