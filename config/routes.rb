66e24dda48629e9faed39967af058a9450dedd79
Volmer <git@radicaos.com>
2014-01-07 13:44:43 -0200

    Index action.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 BootsyDemo::Application.routes.draw do
+  root 'home#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
